96c197c1c2d9ad4bb11324f0190106d501ae03ca
edcombs <evandcombs@gmail.com>
2017-12-12 18:21:03 -0500

    Main-Page: Kenya
    Kenya Layout finished
    Still need to figure out how to get text with links.

--- FoxRiver/FoxRiver/FoxRiver/MainPage.xaml.cs
+++ FoxRiver/FoxRiver/FoxRiver/MainPage.xaml.cs
@@ -9,14 +9,23 @@ namespace FoxRiver
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
+        private string KenyaContent = "<html><head><style>body {font-family: 'Roboto', sans-serif;}p{color: #FFFFFF;font-size: 28pt;font-weight: bold;text-align: center;}a{color: #56C4C2;font-size: 28pt;font-weight: bold;text-align: center;}</style></head><body><p>Through <a href=''>Fox River</a> and<a href=''>MANNA</a></br><a href=''>Worldwide</a>, God is providing</br>primary education and nutrition</br>centers in the villages of</br><a href =''>Bomani</a> and <a href=''>Vipingo</a></p></body></html>";
+        #region Initialization
+        public MainPage()
 		{
             NavigationPage.SetHasNavigationBar(this, false);
             InitializeComponent();
 		}
-        async void Button_Clicked(object sender, EventArgs e)
+        #endregion
+        #region Interactions
+        async void SponsorChildrenClicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new DatabasePage());
         }
+        async void ChangeALifeClicked(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("http://foxrivermissions.org/join"));
+        }
+        #endregion
     }
 }
